3842c3432fe66920cac6d86a7162e15cade5777c
kclem <kendellclement@gmail.com>
2019-11-12 17:50:35 -0500

    add CRISPOR examples

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+import sys
+
+if sys.version_info[0] == 2:
+    import ConfigParser as configparser
+else:
+    import configparser
+
 setup(name='GEM',
-        version='1.0',
+        version='1.1',
         package_dir = {'': 'src'},
         py_modules=['GEM','GEM_CRISPOR','GEM_CRISPRESSO'],
+        install_requires=[
+            'jsonschema',
+            'json2html'
+
+        ]
         )
--- src/GEM.py
+++ src/GEM.py
@@ -3,6 +3,8 @@ import os
 import shutil
 import tarfile
 import tempfile
+from jsonschema import validate
+from json2html import json2html
 
 class GEM_Error(Exception):
     pass
@@ -29,7 +31,7 @@ class GEM:
                     try:
                         metadata = json.load(meta)
                     except json.decoder.JSONDecodeError as e:
-                        raise GEM_Error("Corrupted GEM file. Metadata file cannot be read") from e
+                        raise GEM_Error("Corrupted GEM file. Metadata file cannot be read")
 
 
         self.metadata = metadata
@@ -85,19 +87,21 @@ class GEM:
             gem_filepath = os.path.basename(local_filepath)
 
         if os.path.exists(self._gemfile_location):
-            with tempfile.TemporaryDirectory() as tempdir:
-                tmp_path = os.path.join(tempdir, 'tmp.tar.gz')
+            tempdir = tempfile.mkdtemp()
 
-                with tarfile.open(self._gemfile_location, "r:gz") as old_tar:
-                    with tarfile.open(tmp_path, "w:gz") as new_tar:
-                        for member in old_tar:
-                            if member.name != gem_filepath:
-                                new_tar.addfile(member, old_tar.extractfile(member.name))
-                            else:
-                                print('Replacing previous file named ' + gem_filepath)
-                        new_tar.add(local_filepath,arcname=gem_filepath)
+            tmp_path = os.path.join(tempdir, 'tmp.tar.gz')
 
-                shutil.copy(tmp_path, self._gemfile_location)
+            with tarfile.open(self._gemfile_location, "r:gz") as old_tar:
+                with tarfile.open(tmp_path, "w:gz") as new_tar:
+                    for member in old_tar:
+                        if member.name != gem_filepath:
+                            new_tar.addfile(member, old_tar.extractfile(member.name))
+                        else:
+                            print('Replacing previous file named ' + gem_filepath)
+                    new_tar.add(local_filepath,arcname=gem_filepath)
+
+            shutil.copy(tmp_path, self._gemfile_location)
+            shutil.rmtree(tempdir)
 
         else:
             with tarfile.open(self._gemfile_location,"w:gz") as tar_handle:
@@ -112,19 +116,20 @@ class GEM:
 
 
         if os.path.exists(self._gemfile_location):
-            with tempfile.TemporaryDirectory() as tempdir:
-                tmp_path = os.path.join(tempdir, 'tmp.tar.gz')
+            tempdir = tempfile.mkdtemp()
+            tmp_path = os.path.join(tempdir, 'tmp.tar.gz')
 
-                with tarfile.open(self._gemfile_location, "r:gz") as old_tar:
-                    if gem_filepath not in old_tar.getnames():
-                        raise GEM_Error("GEM file does not contain " + gem_filepath)
+            with tarfile.open(self._gemfile_location, "r:gz") as old_tar:
+                if gem_filepath not in old_tar.getnames():
+                    raise GEM_Error("GEM file does not contain " + gem_filepath)
 
-                    with tarfile.open(tmp_path, "w:gz") as new_tar:
-                        for member in old_tar:
-                            if member.name != gem_filepath:
-                                new_tar.addfile(member, old_tar.extractfile(member.name))
+                with tarfile.open(tmp_path, "w:gz") as new_tar:
+                    for member in old_tar:
+                        if member.name != gem_filepath:
+                            new_tar.addfile(member, old_tar.extractfile(member.name))
 
-                shutil.copy(tmp_path, self._gemfile_location)
+            shutil.copy(tmp_path, self._gemfile_location)
+            shutil.rmtree(tempdir)
 
 
     def get_files(self):
@@ -170,6 +175,282 @@ class GEM:
             with open(tf.name, 'w') as fd:
                 json.dump(new_metadata,fd)
             self.add_file(tf.name,"meta.json")
+
+    def create_html(self,output_file=None):
+        ''' prints html version to view GEM contents'''
+        html_path = self._gemfile_location + ".html"
+        if output_file is not None:
+            html_path = output_file
+        with open(html_path,"w") as html_out:
+            html_out.write("""<!doctype html>
+<html lang="en">
+  <head>
+    <script type='text/javascript' src="https://raw.githubusercontent.com/caldwell/renderjson/master/renderjson.js"></script>
+    <script>
+// Copyright © 2013-2017 David Caldwell <david@porkrind.org>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+// OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+// CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+// Usage
+// -----
+// The module exports one entry point, the `renderjson()` function. It takes in
+// the JSON you want to render as a single argument and returns an HTML
+// element.
+//
+// Options
+// -------
+// renderjson.set_icons("+", "-")
+//   This Allows you to override the disclosure icons.
+//
+// renderjson.set_show_to_level(level)
+//   Pass the number of levels to expand when rendering. The default is 0, which
+//   starts with everything collapsed. As a special case, if level is the string
+//   "all" then it will start with everything expanded.
+//
+// renderjson.set_max_string_length(length)
+//   Strings will be truncated and made expandable if they are longer than
+//   `length`. As a special case, if `length` is the string "none" then
+//   there will be no truncation. The default is "none".
+//
+// renderjson.set_sort_objects(sort_bool)
+//   Sort objects by key (default: false)
+//
+// renderjson.set_replacer(replacer_function)
+//   Equivalent of JSON.stringify() `replacer` argument when it's a function
+//
+// renderjson.set_collapse_msg(collapse_function)
+//   Accepts a function (len:number):string => {} where len is the length of the
+//   object collapsed.  Function should return the message displayed when a
+//   object is collapsed.  The default message is "X items"
+//
+// renderjson.set_property_list(property_list)
+//   Equivalent of JSON.stringify() `replacer` argument when it's an array
+//
+// Theming
+// -------
+// The HTML output uses a number of classes so that you can theme it the way
+// you'd like:
+//     .disclosure    ("⊕", "⊖")
+//     .syntax        (",", ":", "{", "}", "[", "]")
+//     .string        (includes quotes)
+//     .number
+//     .boolean
+//     .key           (object key)
+//     .keyword       ("null", "undefined")
+//     .object.syntax ("{", "}")
+//     .array.syntax  ("[", "]")
+
+var module, window, define, renderjson=(function() {
+    var themetext = function(/* [class, text]+ */) {
+        var spans = [];
+        while (arguments.length)
+            spans.push(append(span(Array.prototype.shift.call(arguments)),
+                              text(Array.prototype.shift.call(arguments))));
+        return spans;
+    };
+    var append = function(/* el, ... */) {
+        var el = Array.prototype.shift.call(arguments);
+        for (var a=0; a<arguments.length; a++)
+            if (arguments[a].constructor == Array)
+                append.apply(this, [el].concat(arguments[a]));
+            else
+                el.appendChild(arguments[a]);
+        return el;
+    };
+    var prepend = function(el, child) {
+        el.insertBefore(child, el.firstChild);
+        return el;
+    }
+    var isempty = function(obj, pl) { var keys = pl || Object.keys(obj);
+                                      for (var i in keys) if (Object.hasOwnProperty.call(obj, keys[i])) return false;
+                                      return true; }
+    var text = function(txt) { return document.createTextNode(txt) };
+    var div = function() { return document.createElement("div") };
+    var span = function(classname) { var s = document.createElement("span");
+                                     if (classname) s.className = classname;
+                                     return s; };
+    var A = function A(txt, classname, callback) { var a = document.createElement("a");
+                                                   if (classname) a.className = classname;
+                                                   a.appendChild(text(txt));
+                                                   a.href = '#';
+                                                   a.onclick = function(e) { callback(); if (e) e.stopPropagation(); return false; };
+                                                   return a; };
+
+    function _renderjson(json, indent, dont_indent, show_level, options) {
+        var my_indent = dont_indent ? "" : indent;
+
+        var disclosure = function(open, placeholder, close, type, builder) {
+            var content;
+            var empty = span(type);
+            var show = function() { if (!content) append(empty.parentNode,
+                                                         content = prepend(builder(),
+                                                                           A(options.hide, "disclosure",
+                                                                             function() { content.style.display="none";
+                                                                                          empty.style.display="inline"; } )));
+                                    content.style.display="inline";
+                                    empty.style.display="none"; };
+            append(empty,
+                   A(options.show, "disclosure", show),
+                   themetext(type+ " syntax", open),
+                   A(placeholder, null, show),
+                   themetext(type+ " syntax", close));
+
+            var el = append(span(), text(my_indent.slice(0,-1)), empty);
+            if (show_level > 0 && type != "string")
+                show();
+            return el;
+        };
+
+        if (json === null) return themetext(null, my_indent, "keyword", "null");
+        if (json === void 0) return themetext(null, my_indent, "keyword", "undefined");
+
+        if (typeof(json) == "string" && json.length > options.max_string_length)
+            return disclosure('"', json.substr(0,options.max_string_length)+" ...", '"', "string", function () {
+                return append(span("string"), themetext(null, my_indent, "string", JSON.stringify(json)));
+            });
+
+        if (typeof(json) != "object" || [Number, String, Boolean, Date].indexOf(json.constructor) >= 0) // Strings, numbers and bools
+            return themetext(null, my_indent, typeof(json), JSON.stringify(json));
+
+        if (json.constructor == Array) {
+            if (json.length == 0) return themetext(null, my_indent, "array syntax", "[]");
+
+            return disclosure("[", options.collapse_msg(json.length), "]", "array", function () {
+                var as = append(span("array"), themetext("array syntax", "[", null, "\\n"));
+                for (var i=0; i<json.length; i++)
+                    append(as,
+                           _renderjson(options.replacer.call(json, i, json[i]), indent+"    ", false, show_level-1, options),
+                           i != json.length-1 ? themetext("syntax", ",") : [],
+                           text("\\n"));
+                append(as, themetext(null, indent, "array syntax", "]"));
+                return as;
+            });
+        }
+
+        // object
+        if (isempty(json, options.property_list))
+            return themetext(null, my_indent, "object syntax", "{}");
+
+        return disclosure("{", options.collapse_msg(Object.keys(json).length), "}", "object", function () {
+            var os = append(span("object"), themetext("object syntax", "{", null, "\\n"));
+            for (var k in json) var last = k;
+            var keys = options.property_list || Object.keys(json);
+            if (options.sort_objects)
+                keys = keys.sort();
+            for (var i in keys) {
+                var k = keys[i];
+                if (!(k in json)) continue;
+                append(os, themetext(null, indent+"    ", "key", '"'+k+'"', "object syntax", ': '),
+                       _renderjson(options.replacer.call(json, k, json[k]), indent+"    ", true, show_level-1, options),
+                       k != last ? themetext("syntax", ",") : [],
+                       text("\\n"));
+            }
+            append(os, themetext(null, indent, "object syntax", "}"));
+            return os;
+        });
+    }
+
+    var renderjson = function renderjson(json)
+    {
+        var options = new Object(renderjson.options);
+        options.replacer = typeof(options.replacer) == "function" ? options.replacer : function(k,v) { return v; };
+        var pre = append(document.createElement("pre"), _renderjson(json, "", false, options.show_to_level, options));
+        pre.className = "renderjson";
+        return pre;
+    }
+    renderjson.set_icons = function(show, hide) { renderjson.options.show = show;
+                                                  renderjson.options.hide = hide;
+                                                  return renderjson; };
+    renderjson.set_show_to_level = function(level) { renderjson.options.show_to_level = typeof level == "string" &&
+                                                                                        level.toLowerCase() === "all" ? Number.MAX_VALUE
+                                                                                                                      : level;
+                                                     return renderjson; };
+    renderjson.set_max_string_length = function(length) { renderjson.options.max_string_length = typeof length == "string" &&
+                                                                                                 length.toLowerCase() === "none" ? Number.MAX_VALUE
+                                                                                                                                 : length;
+                                                          return renderjson; };
+    renderjson.set_sort_objects = function(sort_bool) { renderjson.options.sort_objects = sort_bool;
+                                                        return renderjson; };
+    renderjson.set_replacer = function(replacer) { renderjson.options.replacer = replacer;
+                                                   return renderjson; };
+    renderjson.set_collapse_msg = function(collapse_msg) { renderjson.options.collapse_msg = collapse_msg;
+                                                           return renderjson; };
+    renderjson.set_property_list = function(prop_list) { renderjson.options.property_list = prop_list;
+                                                         return renderjson; };
+    // Backwards compatiblity. Use set_show_to_level() for new code.
+    renderjson.set_show_by_default = function(show) { renderjson.options.show_to_level = show ? Number.MAX_VALUE : 0;
+                                                      return renderjson; };
+    renderjson.options = {};
+    renderjson.set_icons('⊕', '⊖');
+    renderjson.set_show_by_default(false);
+    renderjson.set_sort_objects(false);
+    renderjson.set_max_string_length("none");
+    renderjson.set_replacer(void 0);
+    renderjson.set_property_list(void 0);
+    renderjson.set_collapse_msg(function(len) { return len + " item" + (len==1 ? "" : "s") })
+    return renderjson;
+})();
+
+if (define) define({renderjson:renderjson})
+else (module||{}).exports = (window||{}).renderjson = renderjson;
+</script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>"""+self._gemfile_location+"""</title>
+  </head>
+  <body>
+  <div class='container'>
+      <div class='card my-3'>
+          <div class='card-header'>
+              <h1> GEM Metadata</h1>
+          </div>
+          <div class='card-body'>
+          <div id='metadata_json'></div>
+          </div>
+      </div>
+  <script>
+      document.getElementById("metadata_json").appendChild(
+                  renderjson("""+json.dumps(self.metadata)+""")
+                      );
+      </script>""")
+            html_out.write('<div class="card my-3"><div class="card-header"><h1>Data files</h1></div><div class="card-body"><ul>')
+            files = self.get_files()
+            file_count = len(files)
+            if file_count > 0:
+                for f in files:
+                    html_out.write('<li><a href="">'+f+'</a></li>')
+                html_out.write('</ul>')
+
+
+            html_out.write("""
+            </ul>
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>""")
+            print('wrote ' +  html_path)
+
 
 
 
